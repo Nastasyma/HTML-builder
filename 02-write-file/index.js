7684b7f117e8a019645e698cae06e3b51395a913
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-05-03 22:39:41 +0300

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const { stdin, stdout } = process;
+const file = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+stdout.write('Please, enter text here:\n');
+
+const exit = () => {
+  stdout.write('See you later!');
+  process.exit();
+}
+
+process.on("SIGINT", exit);
+
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    exit();
+  }
+  file.write(data);
+});
