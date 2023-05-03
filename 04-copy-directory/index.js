61a6073d3cfdf94559d6683c70d0bc9acb8d6458
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-05-04 00:04:36 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,72 @@
+const fs = require('fs');
+const path = require('path');
 
+const srcFolder = path.join(__dirname, 'files');
+const destFolder = path.join(__dirname, 'files-copy');
+
+fs.mkdir(destFolder, { recursive: true }, err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('The folder "files-copy" has been created');
+});
+
+fs.readdir(srcFolder, (err, files) => {
+
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  for (let i=0; i<files.length; i++) {
+    const srcFile = path.join(__dirname, 'files', files[i]);
+    const destFile = path.join(__dirname, 'files-copy', files[i]);
+
+    fs.stat(srcFile, (err, stats) => {
+
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      if (!stats.isFile()) {
+        return;
+      } else {
+        fs.copyFile(srcFile, destFile, (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(`${files[i]} was copied to "files-copy" folder`);
+        });
+      }
+    });
+  }
+});
+
+fs.readdir(destFolder, (err, files) => {
+
+  if (err) {
+    console.error(err);
+    return;
+  }
+  for (let i=0; i<files.length; i++) {
+    const srcFile = path.join(__dirname, 'files', files[i]);
+    const destFile = path.join(__dirname, 'files-copy', files[i]);
+    // console.log("Current file: ", destFile)
+
+    fs.access(srcFile, function(err){
+      if (err) {
+        // console.log(`${files[i]} not found`);
+        fs.unlink(destFile, err => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(`${files[i]} has been deleted`);
+        });
+      }
+    });
+  }
+});
